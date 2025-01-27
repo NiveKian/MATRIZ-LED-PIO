589514fe750212625385565c3e31b1b8a43e4d07
Rayane Queiroz <rayaneqsp2@gmail.com>
2025-01-27 18:57:00 -0300

    adicionando minha animação

--- src/Matriz_Led_PIO.c
+++ src/Matriz_Led_PIO.c
@@ -62,7 +62,8 @@ int main()
       startAnimation(animation_type2);
       break;
     case '3':
-      //
+       printf("Animação iniciada: Rayane Queiroz.\n");
+      startAnimation(animation_type3);
       break;
     case '4':
       //
